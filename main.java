84911ce647e3a3678c803e4c62af9dbe2447a15a
YoussefASalama <youssef.ahmed.salama@gmail.com>
2024-01-18 14:36:37 +0800

    Youssef signing out

---
+++ main.java
@@ -0,0 +1,20 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame login;
+        JPanel panel;
+        JLabel lblUsername;
+        JLabel lblPassword;
+        JTextField txtUsername;
+        JTextField txtPassword;
+        JButton btnSignin;
+        JButton btnExit;
+
+        
+    }
+}
