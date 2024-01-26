7d6ff679b8f4cd147d631eb18fd06d08de806c21
YoussefASalama <youssef.ahmed.salama@gmail.com>
2024-01-26 16:40:35 +0800

    changes to login page

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,21 +20,59 @@ public class main {
         JPasswordField pasPassword = new JPasswordField();
         JButton btnSignin = new JButton();
         JButton btnExit = new JButton();
-        Font font = new Font("verdana", Font.PLAIN, 24);
+        Font font = new Font("verdana", Font.PLAIN, 20);
         
+        // Username label
+
         lblUsername.setText("Username:");
-        lblUsername.setBounds(50, 100, 200, 20);
+        lblUsername.setBounds(100, 100, 200, 20);
         lblUsername.setFont(font);
+
+        // Password label
+
         lblPassword.setText("Password:");
-        lblPassword.setBounds(50, 150, 200, 20);
+        lblPassword.setBounds(100, 150, 200, 20);
         lblPassword.setFont(font);
 
-        txtUsername.setBounds(200, 100, 100, 20);
-        pasPassword.setBounds(200, 150, 100, 20);
+        // Sign in button
+
+        btnSignin.setText("Sign In");
+        btnSignin.setBounds(100, 200, 100, 50);
+        btnSignin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = txtUsername.getText();
+                char[] password = pasPassword.getPassword();
+
+                System.out.println("UserName: " + username);
+                for (char letter : password) {
+                    System.out.println(letter);
+                }
+            }
+        });
+
+        // Exit button
+
+        btnExit.setText("Exit");
+        btnExit.setBounds(300, 200, 100, 50);
+        btnExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // Username textbox
+
+        txtUsername.setBounds(250, 100, 150, 20);
+
+        // Password password textbox, I understand the variable name is weird but this is the way
+
+        pasPassword.setBounds(250, 150, 150, 20);
         
+        // login page
+
         login.setLayout(null);
         login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        login.setSize(500, 500);
+        login.setSize(500, 400);
         login.add(lblUsername);
         login.add(lblPassword);
         login.add(pasPassword);
